47994c27250bb7866adb6b994352e2a042e3fdf3
ibnaleem <ibnaleem@outlook.com>
2026-04-05 23:04:41 +0100

    feat(util): add CalculateFileSHA256Hash for streaming file hash computation

--- internal/util/util.go
+++ internal/util/util.go
@@ -2,8 +2,10 @@ package util
 
 import (
 	"os"
+	"io"
 	"fmt"
 	"regexp"
+	"crypto/sha256"
 )
 
 func CheckError(err error) {
@@ -19,4 +21,28 @@ func CheckHash(input string) bool {
 	sha256 := regexp.MustCompile(`^[a-fA-F0-9]{64}$`)
 
 	return md5.MatchString(input) || sha1.MatchString(input) || sha256.MatchString(input)
+}
+
+func CalculateFileSHA256Hash(path string) (string, error) {
+
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	sha256Hasher := sha256.New()
+
+	_, err = io.Copy(sha256Hasher, f)
+
+	if err != nil {
+		return "", err
+	}
+
+	hash := sha256Hasher.Sum(nil)
+
+	return fmt.Sprintf("%x", hash), nil
+
 }
