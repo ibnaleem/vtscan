a7b6eb6a3ba7baa9ad5a9079f01da20bbd8db3d6
ibnaleem <ibnaleem@outlook.com>
2026-04-05 18:13:02 +0100

    feat(client): add HTTP GET method with headers and error handling

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,5 +1,14 @@
 package client
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+var DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3"
+
 type Client struct {
 	APIKey  string
 	BaseURL string
@@ -10,4 +19,39 @@ func NewClient(apikey string) *Client {
 		APIKey: apikey,
 		BaseURL: "https://www.virustotal.com/api/v3",
 	}
+}
+
+func (c *Client) Get(endpoint string) ([]byte, error) {
+
+	client := &http.Client{
+		Timeout: 60 * time.Second,
+	}
+
+	url := fmt.Sprintf("%s/%s", c.BaseURL, endpoint) 
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	
+	if err != nil {
+		return nil, err
+	}	
+
+	req.Header.Set("User-Agent", DefaultUserAgent)
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("x-apikey", c.APIKey)
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}	
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}	
+
+	return body, nil
+
 }
