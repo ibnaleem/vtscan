587a60d2a214c8962cb41bc0281e1db6d01f3405
ibnaleem <ibnaleem@outlook.com>
2026-04-05 22:56:45 +0100

    feat: create CheckHash function to determine if arg is a hash
    - this will help vtscan determine if the arg to subcommand file is either a hash or a path

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,6 +3,7 @@ package util
 import (
 	"os"
 	"fmt"
+	"regexp"
 )
 
 func CheckError(err error) {
@@ -10,4 +11,12 @@ func CheckError(err error) {
         fmt.Fprintln(os.Stderr, err)
         os.Exit(1)
     }
+}
+
+func CheckHash(input string) bool {
+	md5    := regexp.MustCompile(`^[a-fA-F0-9]{32}$`)
+	sha1   := regexp.MustCompile(`^[a-fA-F0-9]{40}$`)
+	sha256 := regexp.MustCompile(`^[a-fA-F0-9]{64}$`)
+
+	return md5.MatchString(input) || sha1.MatchString(input) || sha256.MatchString(input)
 }
