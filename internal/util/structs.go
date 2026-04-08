127aabce8784e3c2e87cefbba39bbb807ee312d0
ibnaleem <ibnaleem@outlook.com>
2026-04-08 12:41:17 +0100

    refactor: move AnalysisResult struct from ip.go to struct.go

--- internal/util/structs.go
+++ internal/util/structs.go
@@ -4,3 +4,10 @@ type TotalVotes struct {
 	Harmless  int `json:"harmless"`
 	Malicious int `json:"malicious"`
 }
+
+type AnalysisResult struct {
+	Method     string `json:"method"`
+	EngineName string `json:"engine_name"`
+	Category   string `json:"category"`
+	Result     string `json:"result"`
+}
