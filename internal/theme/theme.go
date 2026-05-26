0a116c8773e68712437fd8e7dd1cffd4d9ac8280
ibnaleem <ibnaleem@outlook.com>
2026-05-26 23:18:58 +0100

    feat: create internal/theme package with Theme struct and theme variables

---
+++ internal/theme/theme.go
@@ -0,0 +1,43 @@
+package theme
+
+type Theme struct {
+	Reset     string
+	Bold      string
+	Underline string
+	Red       string
+	Green     string
+	Yellow    string
+	Blue      string
+	Magenta   string
+	Cyan      string
+	White     string
+	Gray      string
+}
+
+var LightTheme = Theme{
+	Reset:     "\033[0m",
+	Bold:      "\033[1m",
+	Underline: "\033[4m",
+	Red:       "\033[31m",
+	Green:     "\033[32m",
+	Yellow:    "\033[33m",
+	Blue:      "\033[34m",
+	Magenta:   "\033[35m",
+	Cyan:      "\033[36m",
+	White:     "\033[37m",
+	Gray:      "\033[90m",
+}
+
+var DarkTheme = Theme{
+	Reset:     "\033[0m",
+	Bold:      "\033[1m",
+	Underline: "\033[4m",
+	Red:       "\033[91m",
+	Green:     "\033[92m",
+	Yellow:    "\033[93m",
+	Blue:      "\033[94m",
+	Magenta:   "\033[95m",
+	Cyan:      "\033[96m",
+	White:     "\033[97m",
+	Gray:      "\033[37m",
+}
