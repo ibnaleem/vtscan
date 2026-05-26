feeff42be547756ccf5298dda4e1396b7cdb96df
ibnaleem <ibnaleem@outlook.com>
2026-05-26 21:57:30 +0100

    feat: add missing PrintIPComments func

--- internal/util/util.go
+++ internal/util/util.go
@@ -487,3 +487,30 @@ func PrintDomainResponse(domainResponse DomainResponse) {
 	}
 	enginesTable.Render()
 }
+
+func PrintIPComments(w io.Writer, ip string, resp IPCommentsResponse) {
+        fmt.Fprintf(w, "Comments for %s (%d on this page)\n\n", ip, resp.Meta.Count)
+
+        if len(resp.Data) == 0 {
+                fmt.Fprintln(w, "No comments found.")
+                return
+        }
+
+        for i, c := range resp.Data {
+                date := time.Unix(c.Attributes.Date, 0).Format("2006-01-02 15:04:05")
+                author := c.Relationships.Author.Data.ID
+                if author == "" {
+                        author = "unknown"
+                }
+                fmt.Fprintf(w, "[%d] %s  by %s\n", i+1, date, author)
+                if len(c.Attributes.Tags) > 0 {
+                        fmt.Fprintf(w, "    Tags: %s\n", strings.Join(c.Attributes.Tags, ", "))
+                }
+                fmt.Fprintf(w, "    Votes: +%d / -%d (abuse: %d)\n", c.Attributes.Votes.Positive, c.Attributes.Votes.Negative, c.Attributes.Votes.Abuse)
+                fmt.Fprintf(w, "    %s\n\n", c.Attributes.Text)
+        }
+
+        if resp.Meta.Cursor != "" {
+                fmt.Fprintln(w, "Note: more comments exist. Pagination not yet implemented.")
+        }
+}
