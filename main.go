9adbf0dd3b93cc2825ad6c2a571753a014d15f4f
ibnaleem <ibnaleem@outlook.com>
2026-05-26 23:55:12 +0100

    fix: replace glamour header with theme.Bold to avoid terminal-width padding

--- main.go
+++ main.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/ibnaleem/vtscan/cmd"
-	"github.com/ibnaleem/vtscan/internal/render"
+	"github.com/ibnaleem/vtscan/internal/theme"
 	"github.com/ibnaleem/vtscan/internal/util"
 )
 
 func main() {
 
-	vtscanTitle := strings.TrimSpace(render.Markdown("**vtscan**"))
-	phraseRender := strings.TrimSpace(render.Markdown(util.GetRandPhrase()))
-
-	fmt.Println(vtscanTitle + " - " + phraseRender)
+	fmt.Println(theme.Bold("vtscan") + " - " + util.GetRandPhrase())
 
 	cmd.Execute()
 
