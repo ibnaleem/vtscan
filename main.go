b5f8b3786aa59860e8c9cb57b183e52cb6e61a7e
ibnaleem <ibnaleem@outlook.com>
2026-05-26 23:51:35 +0100

    fix: render title in cyan and phrase in glamour separately

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ibnaleem/vtscan/cmd"
+	"github.com/ibnaleem/vtscan/internal/render"
+	"github.com/ibnaleem/vtscan/internal/util"
+	"github.com/ibnaleem/vtscan/internal/theme"
 )
 
 func main() {
+
+	fmt.Print(theme.Cyan("vtscan") + " -- " + render.Markdown(util.GetRandPhrase()))
 
 	cmd.Execute()
 
